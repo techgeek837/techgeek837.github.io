ff5e98d64ad267053e8761cfe48cf068969e7dc5
techgeek837 <66062754+techgeek837@users.noreply.github.com>
2020-07-07 23:00:20 +0530

    Add files via upload

--- pages/d3/map.js
+++ pages/d3/map.js
@@ -9,6 +9,30 @@ d3.select('.map')
         .attr('height', height)
     .append('g')
 
+const cases = {
+    Srikakulam:329,
+    Vizianagaram:269,
+    Visakhapatnam:945,
+    Krishna:1898,
+    Guntur:2262,
+    East_Godavari:1890,
+    WestGodavari:1319,
+    Prakasam:767,
+    Kurnool:2671,
+    Anantapur:2481,
+    Nellore:810,
+    Kadapa:1369,
+    Chittoor:1510,
+}
+let max= 0
+for (const key in cases) {
+    max = (max<cases[key]) ? cases[key] : max
+}
+
+
+let color = d3.scaleSequential()
+  .domain([0, max+300])
+  .interpolator(d3.interpolateReds)
 
 
 function plotTopoDist(fileURL){
@@ -34,23 +58,23 @@ function plotTopoDist(fileURL){
                 .data(geoJson.features)
                 .join('path')
                     .attr('d',data=>pathGen(data))
-                    .attr('stroke-width','0.06')
-                    .attr("fill", "#62E69D")
+                    .attr('stroke-width','0.02')
+                    .attr("fill", d=>color(cases[d.properties.district]))
                     .attr("stroke", "#222")
                     .attr('transform',`translate(${translate[0]},${translate[1]}),scale(${scale})`)
                     .on('mousemove',function(d){
                         d3.select(this)
-                        .attr('fill','blue')
+                        .attr('fill',d=>color(cases[d.properties.district]+200))
                         let name = d.properties.district
                         if(name){
                             d3.select('#tooltip')
-                            .text(`${name}`)
+                            .html(`${name}<p>${cases[name]}</p>`)
                             .style("left", (d3.event.pageX + 10) + "px")
                             .style("top", (d3.event.pageY - 15) + "px");
                         }
                     })
                     .on('mouseout',function(){
-                        d3.select(this).attr('fill','#62E69D')
+                        d3.select(this).attr('fill',d=>color(cases[d.properties.district]))
                         d3.select('#tooltip')
                             .text("")
                     })
@@ -133,7 +157,7 @@ function plotTopoWorld(fileURL){
                     .attr('stroke-width','1.3')
                     .attr("fill", "#9e1fde")
                     .attr("stroke", 'white')
-                    .attr('transform',`translate(${translate[0]},${translate[1]}),scale(${scale})`)
+                    .attr('transform',`translate(${translate[0]},${translate[1]+50}),scale(${scale})`)
                     .on('mousemove',function(d){
                         d3.select(this)
                         .attr('fill','blue')
@@ -144,3 +168,6 @@ function plotTopoWorld(fileURL){
         }
     )
 }
+
+
+plotTopoDist('./Maps/TopoJson/andhrapradesh.topo.json')
